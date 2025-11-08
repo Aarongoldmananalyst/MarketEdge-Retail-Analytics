f057b799b6cb3144a1b71f1274cf8c3e71ded4b2
Aaron Goldman <82747526+Aarongoldmananalyst@users.noreply.github.com>
2025-11-07 17:10:24 -0700

    Create fact_sales_margin.sql

---
+++ models/marts/fact_sales_margin.sql
@@ -0,0 +1,14 @@
+{{ config(materialized='table') }}
+with s as (
+    select * from {{ ref('stg_sales') }}
+)
+select 
+    store,
+    category,
+    sum(revenue) as total_revenue,
+    sum(cost) as total_cost,
+    sum(margin) as total_margin,
+    round(sum(margin)/sum(revenue)*100,2) as margin_pct
+from s
+group by store, category
+order by total_margin desc
