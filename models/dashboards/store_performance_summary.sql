c77cbb4c90cb27ce3aaf4bcf9a9c8cabeb0f6b86
Aaron Goldman <82747526+Aarongoldmananalyst@users.noreply.github.com>
2025-11-08 12:18:52 -0700

    Delete models directory

--- models/dashboards/store_performance_summary.sql
+++
@@ -1,9 +0,0 @@
-{{ config(materialized='table') }}
-select 
-    store,
-    sum(total_revenue) as revenue,
-    sum(total_margin) as profit,
-    avg(margin_pct) as avg_margin
-from {{ ref('fact_sales_margin') }}
-group by store
-order by profit desc
